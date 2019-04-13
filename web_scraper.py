0c66650a97d8cff4fedcf91b6692da98505b55de
Raphael <raphael.couronne@gmail.com>
2019-04-13 16:19:09 +0200

    Initial commit

---
+++ web_scraper.py
@@ -0,0 +1,18 @@
+from urlparse import urljoin
+from bs4 import BeautifulSoup
+import requests
+
+
+BASE_URL = "http://genius.com"
+artist_url = "http://genius.com/artists/Andre-3000/"
+
+response = requests.get(artist_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
+
+soup = BeautifulSoup(response.text, "lxml")
+for song_link in soup.select('ul.song_list > li > a'):
+    link = urljoin(BASE_URL, song_link['href'])
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text)
+    lyrics = soup.find('div', class_='lyrics').text.strip()
+
+    # tokenize `lyrics` with nltk
