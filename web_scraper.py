27e52b4fa3ac5526310cbba2f8be60f87e64e745
Raphael <raphael.couronne@gmail.com>
2019-04-13 18:19:39 +0200

    Scraper

--- web_scraper.py
+++ web_scraper.py
@@ -1,18 +1,92 @@
-from urlparse import urljoin
-from bs4 import BeautifulSoup
-import requests
+import json
+import os
+import numpy as np
+
+with open("secret.json") as json_file:
+    secret = json.load(json_file)
 
 
-BASE_URL = "http://genius.com"
-artist_url = "http://genius.com/artists/Andre-3000/"
 
-response = requests.get(artist_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
+#%% Get rappers
 
-soup = BeautifulSoup(response.text, "lxml")
-for song_link in soup.select('ul.song_list > li > a'):
-    link = urljoin(BASE_URL, song_link['href'])
-    response = requests.get(link)
-    soup = BeautifulSoup(response.text)
-    lyrics = soup.find('div', class_='lyrics').text.strip()
+from urllib.request import urlopen
+import bs4 as BeautifulSoup
+import unidecode
+html = urlopen('https://fr.wikipedia.org/wiki/Cat%C3%A9gorie:Rappeur_fran%C3%A7ais').read()
 
-    # tokenize `lyrics` with nltk
+with open("rappeurs.html") as html:
+    soup = BeautifulSoup.BeautifulSoup(html)
+soup = soup.find_all('li')
+
+
+
+
+
+def filter_rapper_name(name):
+
+    suffix_labels = [' (rappeur)', " (artiste)",
+              " (rappeur français)", "/Brouillon",
+              ' (chanteur)',' (musicien)']
+    for suffix_label in suffix_labels:
+        name = name.split(suffix_label)[0]
+
+
+    prefix_labels = ['Utilisateur:','MarkHunt/']
+    for prefix_label in prefix_labels:
+        if len(name.split(prefix_label))>1:
+            name = name.split(prefix_label)[1]
+
+    return name
+
+artists_name_scraped=[]
+for line in soup:
+    artists_name_scraped.append(unidecode.unidecode(filter_rapper_name(line.find_all('a')[0].contents[0]).lower()))
+
+
+#%%
+
+import lyricsgenius
+genius = lyricsgenius.Genius(secret['client_access_token'])
+genius._SLEEP_MIN = 0.2
+genius.sleep_time = 0.2
+
+
+artists_done = ['disiz la peste','nekfeu']
+
+artists_name_written = ['lomepal','iam','romeo elvis','pnl','jul','vald','damso','niska','booba','kaaris','la fouine','lorenzo',
+                'kalash criminel','gradur','lacrim','mhd','casseurs flowters', 'dosseh','orelsan']
+
+artists_name = np.unique(artists_name_written+artists_name_scraped)
+
+#%%
+
+
+
+
+for artist_name in artists_name:
+    try:
+        artist = genius.search_artist(artist_name, max_songs=500, sort="title")
+        for song in artist.songs:
+            if not os.path.exists(os.path.join("data",artist.name)):
+                os.mkdir(os.path.join("data",artist.name))
+
+            try:
+                song.save_lyrics(os.path.join("data",artist.name,song.title), overwrite='y')
+            except:
+                print("Couldnt save : "+song.title)
+    except:
+        print("Couldnt load artist : " + artist_name)
+
+
+
+
+"""
+base_url = "http://api.genius.com"
+headers = {'Authorization': "Bearer "+secret['client_access_token']}
+#headers = {'Authorization': secret['client_secret']}
+search_url = base_url + "/search"
+song_title = "Back"
+#artist_name = "Alkpote"
+data = {'q': song_title}
+response = requests.get(search_url, data=data, headers=headers)
+"""
